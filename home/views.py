92973dec377c7897dedc2db28c1a7cecb6b7f529
joshcallawaydev <joshrcal89@gmail.com>
2022-03-25 20:04:51 +0000

    Added wireframe / design images and README folder

---
+++ home/views.py
@@ -0,0 +1,10 @@
+"""A module to build the views"""
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    """A view to return the index page"""
+
+    return render(request, 'home/index.html')
