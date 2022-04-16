23c0b9f4dd44e9092d06b465f13b15abbef76090
joshcallawaydev <joshrcal89@gmail.com>
2022-04-16 17:29:20 +0000

    Reformat

--- products/models.py
+++ products/models.py
@@ -30,11 +30,11 @@ class Product(models.Model):
     """
     products class
     """
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=250, null=True, blank=True)
     name = models.CharField(max_length=250)
     description = models.TextField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1035, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
