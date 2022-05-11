52f101699cd0edf9b969e86930ab33f5a354c979
joshcallawaydev <joshrcal89@gmail.com>
2022-05-11 20:23:16 +0000

    Added html for checkout and linked url to project

---
+++ checkout/urls.py
@@ -0,0 +1,7 @@
+"""checkout URL Configuration"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.checkout, name='checkout')
+]
--- checkout/views.py
+++ checkout/views.py
@@ -1,1 +1,23 @@
 """ views"""
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages
+from .forms import OrderForm
+
+
+def checkout(request):
+    """ checkout view """
+
+    basket = request.session.get('basket', {})
+    print(basket)
+
+    if not basket:
+        messages.error(request, "There's nothing in your basket")
+        return redirect(reverse('basket'))
+
+    order_form = OrderForm()
+    print(order_form)
+
+    template = "checkout/checkout.html"
+
+    context = {'order_form': order_form}
+    return render(request, template, context)
