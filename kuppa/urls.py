40641ceb9acc1a3d6150e62ae12f2ffd2476bce4
joshcallawaydev <joshrcal89@gmail.com>
2022-05-29 20:08:38 +0000

    Added favicon

--- kuppa/urls.py
+++ kuppa/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,6 @@ urlpatterns = [
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
     path('account/', include('accounts.urls')),
+    path('favicon.ico', RedirectView.as_view(
+        url=staticfiles_storage.url('media/favicon.ico')))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
