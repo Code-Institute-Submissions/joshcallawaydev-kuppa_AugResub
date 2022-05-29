de73e69e156a2c2d3a0649d4413939a42a7d1456
joshcallawaydev <joshrcal89@gmail.com>
2022-05-29 20:24:51 +0000

    Trying new settings to get heroku to deploy

--- kuppa/settings.py
+++ kuppa/settings.py
@@ -12,14 +12,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 
 import os
-from pathlib import Path
 from dotenv import load_dotenv
 import dj_database_url
 
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
@@ -27,7 +27,7 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8(2=d3&^evrr+dd%x_vw_qra2n*@oa%t7z-*x!-xwrhm-oa(f-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
