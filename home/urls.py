9fd7f8adcaef68a5d432d76966f6003e8afebf60
joshcallawaydev <joshrcal89@gmail.com>
2022-05-30 09:49:05 +0000

    Added links to privacy and terms

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('terms/', views.terms_of_server, name='terms_of_server'),
+    path('privacy/', views.privacy_policy, name='privacy_policy')
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,13 @@ def index(request):
     """A view to return the index page"""
 
     return render(request, 'home/index.html')
+
+
+def terms_of_server(request):
+    """ a page for the site terms of service"""
+    return render(request, 'home/terms_of_server.html')
+
+
+def privacy_policy(request):
+    """link to privacy policy"""
+    return render(request, 'home/privacy_policy.html')
