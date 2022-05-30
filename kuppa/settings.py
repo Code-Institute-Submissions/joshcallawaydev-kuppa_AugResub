5f0e88fedc091da0ac819cf8225b99d257419d14
joshcallawaydev <joshrcal89@gmail.com>
2022-05-30 23:13:21 +0000

    Manually setting debug to false for examiners view

--- kuppa/settings.py
+++ kuppa/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['kuppa-app.herokuapp.com', 'localhost']
 
