43fbac42839d403d3cb4ce98a3ff54c919572468
joshcallawaydev <joshrcal89@gmail.com>
2022-05-30 12:49:22 +0000

    Updated typos

--- kuppa/settings.py
+++ kuppa/settings.py
@@ -173,6 +173,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # AWS logical
 
 if 'USE_AWS' in os.environ:
@@ -203,14 +212,6 @@ if 'USE_AWS' in os.environ:
 DELIVERY_PERCENTAGE = 10
 DELIVERY_THRESHOLD = 49
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
