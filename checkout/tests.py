6316ed8398bae5269de4d7346581e50d0f632d06
joshcallawaydev <joshrcal89@gmail.com>
2022-05-31 09:51:14 +0000

    Added 2 test cases to checkout

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class CheckoutTestCases(TestCase):
+    """ checkoout tests """
+
+    def test_get_checkout_page(self):
+        """ get checkout 200 """
+        response = self.client.get('checkout/checkout.html')
+        self.assertEqual(200, response.status)
+
+    def test_get_checkout_complete_page(self):
+        """ get checkout complete 200 """
+        response = self.client.get('checkout/checkout_complete.html')
+        self.assertEqual(200, response.status)
