208558591eb4994295910e3d5b1824d2110a55b6
joshcallawaydev <joshrcal89@gmail.com>
2022-08-14 13:26:02 +0000

    Added new model for tracking users fav products - future iterations will use this for subs

--- favourites/admin.py
+++ favourites/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import ProductFavourite
 
-# Register your models here.
+
+class FavouritesAdmin(admin.ModelAdmin):
+    """set field names and display in admin dashboard"""
+    list_display = (
+        'user',
+        'favourite_category',
+        'favourite_product',
+        'order_frequency',
+    )
+
+
+admin.site.register(ProductFavourite, FavouritesAdmin)
--- favourites/models.py
+++ favourites/models.py
@@ -1,1 +1,31 @@
 """ favourites model """
+from django.db import models
+from django.contrib.auth.models import User
+from products.models import Category, Product
+
+
+class ProductFavourite(models.Model):
+    """
+    table for building knowledge of customers fav products
+    could be used in the future to set up subscriptions
+    """
+
+    class OrderFrequency(models.IntegerChoices):
+        """ Integer Choices """
+        ONE_WEEK = 1
+        TWO_WEEK = 2
+        THREE_WEEKS = 3
+        FOUR_WEEKS = 4
+
+    user = models.OneToOneField(User, null=True, on_delete=models.SET_NULL)
+    favourite_category = models.ForeignKey(
+        Category, null=True, on_delete=models.SET_NULL)
+    favourite_product = models.ForeignKey(
+        Product, null=True, on_delete=models.SET_NULL)
+    order_frequency = models.IntegerField(choices=OrderFrequency.choices)
+
+    def __str__(self):
+        return self.user.username
+
+    # def __str__(self):
+    #     return self.favourite_category.name
