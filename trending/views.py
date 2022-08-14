eb51e04522081452cfdd02430324a3cebb4126da
joshcallawaydev <joshrcal89@gmail.com>
2022-08-14 11:25:17 +0000

    Added trending products logic and built out html. All prints removed

--- trending/views.py
+++ trending/views.py
@@ -1,11 +1,37 @@
 """ Module for trending products """
 from django.shortcuts import render
+from products.models import Product
 
 
 def trending(request):
     """ function to show trending products"""
 
+    products = Product.objects.all()
+    item = {}
+    all_products = []
+    trending_products = []
+
+    for product in products:
+        item["id"] = product.pk
+        item["name"] = product.name
+        item["category"] = product.category
+        item["description"] = product.description
+        item["image"] = product.image
+        item["image_url"] = product.image_url
+        item["price"] = product.price
+        item["rating"] = product.rating
+        item["sku"] = product.sku
+        item["value"] = product.favourites.count()
+        all_products.append(item.copy())
+
+    for each_item in all_products:
+        if each_item["value"] > 0:
+            trending_products.append(each_item.copy())
+
     # context to be passed to html
-    context = {}
+    context = {
+        "all_products": all_products,
+        "trending_products": trending_products,
+    }
 
     return render(request, "trending.html", context)
